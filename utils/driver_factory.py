3631a16ae11cb202ee17fd05b5ea53c077e62c1d
Amine <sabrina@example.com>
2026-02-15 21:51:35 +0100

    chore: Update dependencies and driver factory

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from utils.config import Config
 
 def get_driver():
     """Initializes and returns a Chrome WebDriver instance."""
-    service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     
@@ -14,5 +11,6 @@ def get_driver():
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
         
-    driver = webdriver.Chrome(service=service, options=options)
+    # Selenium 4.10+ handles driver management automatically
+    driver = webdriver.Chrome(options=options)
     return driver
